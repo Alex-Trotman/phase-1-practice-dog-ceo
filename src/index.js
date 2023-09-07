9c41ded721987b2f6d30369e954befa3d63e59b1
Alex Trotman <alextrotman04@gmail.com>
2023-09-07 14:43:59 -0500

    save

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+const imagesContainer = document.getElementById('dog-image-container')
+
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+const dogImageContainer = document.getElementById('dog-image-container')
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch(imgUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then((data) => {
+            data.forEach(function(imageUrl){
+                const img = document.createElement('img')
+                img.src = imgUrl;
+                imagesContainer.appendChild(img)
+            })
+      })
+      .catch(function(error) {
+        console.log("Error fetching images:", error);
+      });
+  });
+
+
+
+
+
+
 console.log('%c HI', 'color: firebrick')
